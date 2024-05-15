d064169135169d592ff00a96752c2b1e02b0d319
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-05-15 15:01:51 -0400

    ahd 0.1.0 -> 0.1.1

--- pkgs/ahd/default.nix
+++ pkgs/ahd/default.nix
@@ -6,13 +6,13 @@
 
 stdenv.mkDerivation rec {
   pname   = "ahd";
-  version = "0.1.0";
+  version = "0.1.1";
 
   src = fetchFromGitHub {
     owner = "ona-li-toki-e-jan-Epiphany-tawa-mi";
     repo  = "AHD";
     rev   = version;
-    hash  = "sha256-LIazsGDAsKdeKD5sOShAA6xVg5P6+UXpQ8SrptPkxfQ=";
+    hash  = "sha256-JCJK0ov0th7Pry6AxeqClgYzRJ1dgV/3cTLhEDIhYe0=";
   };
 
   buildInputs = [ gnuapl ];
