418bbf3d7ee798b57040234fd6822eb5a35f13a3
ona-li-toki-e-jan-Epiphany-tawa-mi <ona-li-toki-e-jan-Epiphany-tawa-mi@protonmail.com>
2024-12-29 18:43:07 -0500

    Added initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "epitaphpkgs Nix package repository";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+  outputs = { nixpkgs, self, ... }:
+    let inherit (nixpkgs.lib) genAttrs systems;
+
+        forAllSystems = f: genAttrs systems.flakeExposed (system: f {
+          pkgs = import nixpkgs { inherit system; };
+        });
+    in {
+      packages = forAllSystems ({ pkgs, ... }:
+        pkgs.callPackage ./default.nix {});
+
+      legacyPackages = self.packages;
+    };
+}
